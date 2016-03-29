1ca1f24053f6f6dad02c935de65ba3de8eae3722
sejour-a <sejour_a@jormungandr.eu>
2016-03-29 17:12:14 +0200

    Login now works!

--- neige_outside/views.py
+++ neige_outside/views.py
@@ -4,9 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from .models import Post
 from django.shortcuts import render_to_response
 from django.template.context_processors import csrf
-
-
-csrf_token = {}
+from django.shortcuts import redirect
 
 
 def error_404(request):
@@ -37,7 +35,8 @@ def neige_outside(request):
     """Template filling"""
     latest_posts = Post.objects.order_by('-pub_date')[:5]
     context = RequestContext(request, {
-        'latest_posts': latest_posts, })
+        'latest_posts': latest_posts,
+        'is_auth': request.user.is_authenticated(), })
 
     """Computed HTML response"""
     return HttpResponse(template.render(context))
@@ -53,7 +52,8 @@ def posts(request, post_id):
     post = Post.objects.filter(id=post_id)
     if post:
         context = RequestContext(request, {
-            'post': post[0], })
+            'post': post[0],
+            'is_auth': request.user.is_authenticated(), })
     else:
         return error_404(request)
 
@@ -63,40 +63,37 @@ def posts(request, post_id):
 def new(request):
     """New post page"""
 
-    csrf_token.update(csrf(request))
-    if request.user.is_authenticated():
-        template = loader.get_template('neige_outside/new_post.html')
-        return HttpResponse(template.render())
+    if not request.user.is_authenticated():
+        return redirect("neige_outside.views.login_view")
 
-    return login_view(request)
+    template = loader.get_template("neige_outside/new_post.html")
+    return HttpResponse(template.render())
 
 
 def login_view(request):
     """Login page"""
 
-    csrf_token.update(csrf(request))
+    if request.user.is_authenticated():
+        template = loader.get_template("neige_outside/logged_in.html")
+        return HttpResponse(template.render())
 
-    return render_to_response('neige_outside/login_view.html', csrf_token)
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
 
+        if user is not None:
+            login(request, user)
+            return redirect("neige_outside.views.neige_outside")
 
-def login(request):
-    """Login"""
-
-    username = request.POST['username']
-    password = request.POST['password']
-
-    user = authenticate(username=username, password=password)
-
-    if user is not None:
-        login(request, user)
-        return neige_outside(request)
-
-    return HttpResponse("login failed")
+    c = {}
+    c.update(csrf(request))
+    return render_to_response("neige_outside/login_view.html", c)
 
 
 def logout_view(request):
     """Logout"""
 
     logout(request)
-    return index(request)
+    return redirect("neige_outside.views.neige_outside")
 
